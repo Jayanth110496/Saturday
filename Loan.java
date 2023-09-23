ebdcc32cc62c37fb8dc905431d99767b7e521fec
Jayanth110496 <jayanth110496@gmail.com>
2023-09-23 06:00:37 +0000

    secondcommit

--- Loan.java
+++ Loan.java
@@ -15,7 +15,7 @@ int size = 2 * n - 1;
 //inner loop 1and2  
 for(int i = 1; i <= size; i++)  
 {  
-//outer loop      
+//outer loop  in 3 and 4    
 for(int j = 1; j <= size; j++)  
 {  
 //calculates and prints the values for pattern  
